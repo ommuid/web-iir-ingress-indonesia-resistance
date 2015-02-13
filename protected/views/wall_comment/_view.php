87aeea5511f19c2360ba9f853b324babbb650790
PutraSudaryanto <putra.sudaryanto@gmail.com>
2015-02-13 13:48:54 +0700

    ommu :: wall comment

---
+++ protected/views/wall_comment/_view.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Ommu Wall Comments (ommu-wall-comment)
+ * @var $this WallcommentController * @var $data OmmuWallComment *
+ * @author Putra Sudaryanto <putra.sudaryanto@gmail.com>
+ * @copyright Copyright (c) 2014 Ommu Platform (ommu.co)
+ * @link http://company.ommu.co
+ * @contect (+62)856-299-4114
+ *
+ */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('comment_id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->comment_id), array('view', 'id'=>$data->comment_id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('publish')); ?>:</b>
+	<?php echo CHtml::encode($data->publish); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('parent_id')); ?>:</b>
+	<?php echo CHtml::encode($data->parent_id); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('wall_id')); ?>:</b>
+	<?php echo CHtml::encode($data->wall_id); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('user_id')); ?>:</b>
+	<?php echo CHtml::encode($data->user_id); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('comment')); ?>:</b>
+	<?php echo CHtml::encode($data->comment); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('creation_date')); ?>:</b>
+	<?php echo CHtml::encode($data->creation_date); ?>
+	<br />
+
+	<?php /*
+	<b><?php echo CHtml::encode($data->getAttributeLabel('modified_date')); ?>:</b>
+	<?php echo CHtml::encode($data->modified_date); ?>
+	<br />
+
+	*/ ?>
+
+</div>
