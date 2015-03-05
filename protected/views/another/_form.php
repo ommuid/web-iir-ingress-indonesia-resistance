57c5063dcb5dec852fb9d29caf45662ae269f5fa
PutraSudaryanto <putra.sudaryanto@gmail.com>
2015-03-05 14:10:15 +0700

    daops :: add another source

---
+++ protected/views/another/_form.php
@@ -0,0 +1,164 @@
+<?php
+/**
+ * Daop Anothers (daop-anothers)
+ * @var $this AnotherController * @var $model DaopAnothers * @var $form CActiveForm
+ *
+ * @author Putra Sudaryanto <putra.sudaryanto@gmail.com>
+ * @copyright Copyright (c) 2014 Ommu Platform (ommu.co)
+ * @link http://company.ommu.co
+ * @contect (+62)856-299-4114
+ *
+ */
+?>
+
+<?php $form=$this->beginWidget('application.components.system.OActiveForm', array(
+	'id'=>'daop-anothers-form',
+	'enableAjaxValidation'=>true,
+	//'htmlOptions' => array('enctype' => 'multipart/form-data')
+)); ?>
+
+<?php //begin.Messages ?>
+<div id="ajax-message">
+	<?php echo $form->errorSummary($model); ?>
+</div>
+<?php //begin.Messages ?>
+
+<fieldset>
+
+	<div class="clearfix publish">
+		<?php echo $form->labelEx($model,'status'); ?>
+		<div class="desc">
+			<?php echo $form->checkBox($model,'status'); ?>
+			<?php echo $form->labelEx($model,'status'); ?>
+			<?php echo $form->error($model,'status'); ?>
+			<?php /*<div class="small-px silent"></div>*/?>
+		</div>
+	</div>
+
+	<div class="clearfix">
+		<?php echo $form->labelEx($model,'another_name'); ?>
+		<div class="desc">
+			<?php echo $form->textField($model,'another_name',array('size'=>60,'maxlength'=>64)); ?>
+			<?php echo $form->error($model,'another_name'); ?>
+			<?php /*<div class="small-px silent"></div>*/?>
+		</div>
+	</div>
+
+	<div class="clearfix">
+		<?php echo $form->labelEx($model,'another_desc'); ?>
+		<div class="desc">
+			<?php echo $form->textArea($model,'another_desc',array('rows'=>6, 'cols'=>50)); ?>
+			<?php echo $form->error($model,'another_desc'); ?>
+			<?php /*<div class="small-px silent"></div>*/?>
+		</div>
+	</div>
+
+	<div class="clearfix">
+		<?php echo $form->labelEx($model,'another_cover'); ?>
+		<div class="desc">
+			<?php echo $form->textField($model,'another_cover',array('size'=>60,'maxlength'=>64)); ?>
+			<?php echo $form->error($model,'another_cover'); ?>
+			<?php /*<div class="small-px silent"></div>*/?>
+		</div>
+	</div>
+
+	<div class="clearfix">
+		<?php echo $form->labelEx($model,'another_photo'); ?>
+		<div class="desc">
+			<?php echo $form->textField($model,'another_photo',array('size'=>60,'maxlength'=>64)); ?>
+			<?php echo $form->error($model,'another_photo'); ?>
+			<?php /*<div class="small-px silent"></div>*/?>
+		</div>
+	</div>
+
+	<div class="clearfix">
+		<?php echo $form->labelEx($model,'country_id'); ?>
+		<div class="desc">
+			<?php echo $form->textField($model,'country_id'); ?>
+			<?php echo $form->error($model,'country_id'); ?>
+			<?php /*<div class="small-px silent"></div>*/?>
+		</div>
+	</div>
+
+	<div class="clearfix">
+		<?php echo $form->labelEx($model,'province_id'); ?>
+		<div class="desc">
+			<?php echo $form->textField($model,'province_id'); ?>
+			<?php echo $form->error($model,'province_id'); ?>
+			<?php /*<div class="small-px silent"></div>*/?>
+		</div>
+	</div>
+
+	<div class="clearfix">
+		<?php echo $form->labelEx($model,'city_id'); ?>
+		<div class="desc">
+			<?php echo $form->textField($model,'city_id',array('size'=>11,'maxlength'=>11)); ?>
+			<?php echo $form->error($model,'city_id'); ?>
+			<?php /*<div class="small-px silent"></div>*/?>
+		</div>
+	</div>
+
+	<div class="clearfix">
+		<?php echo $form->labelEx($model,'users'); ?>
+		<div class="desc">
+			<?php echo $form->textField($model,'users'); ?>
+			<?php echo $form->error($model,'users'); ?>
+			<?php /*<div class="small-px silent"></div>*/?>
+		</div>
+	</div>
+
+	<div class="clearfix">
+		<?php echo $form->labelEx($model,'creation_date'); ?>
+		<div class="desc">
+			<?php echo $form->textField($model,'creation_date'); ?>
+			<?php echo $form->error($model,'creation_date'); ?>
+			<?php /*<div class="small-px silent"></div>*/?>
+		</div>
+	</div>
+
+	<div class="clearfix">
+		<?php echo $form->labelEx($model,'creation_id'); ?>
+		<div class="desc">
+			<?php echo $form->textField($model,'creation_id',array('size'=>11,'maxlength'=>11)); ?>
+			<?php echo $form->error($model,'creation_id'); ?>
+			<?php /*<div class="small-px silent"></div>*/?>
+		</div>
+	</div>
+
+	<div class="clearfix">
+		<?php echo $form->labelEx($model,'modified_date'); ?>
+		<div class="desc">
+			<?php echo $form->textField($model,'modified_date'); ?>
+			<?php echo $form->error($model,'modified_date'); ?>
+			<?php /*<div class="small-px silent"></div>*/?>
+		</div>
+	</div>
+
+	<div class="clearfix">
+		<?php echo $form->labelEx($model,'modified_id'); ?>
+		<div class="desc">
+			<?php echo $form->textField($model,'modified_id',array('size'=>11,'maxlength'=>11)); ?>
+			<?php echo $form->error($model,'modified_id'); ?>
+			<?php /*<div class="small-px silent"></div>*/?>
+		</div>
+	</div>
+
+	<div class="submit clearfix">
+		<label>&nbsp;</label>
+		<div class="desc">
+			<?php echo CHtml::submitButton($model->isNewRecord ? Phrase::trans(1,0) : Phrase::trans(2,0), array('onclick' => 'setEnableSave()')); ?>
+		</div>
+	</div>
+
+</fieldset>
+<?php /*
+<div class="dialog-content">
+</div>
+<div class="dialog-submit">
+	<?php echo CHtml::submitButton($model->isNewRecord ? Phrase::trans(1,0) : Phrase::trans(2,0) ,array('onclick' => 'setEnableSave()')); ?>
+	<?php echo CHtml::button(Phrase::trans(4,0), array('id'=>'closed')); ?>
+</div>
+*/?>
+<?php $this->endWidget(); ?>
+
+
